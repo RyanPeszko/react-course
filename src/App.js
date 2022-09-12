b0a1a0780e24ec722ed77114b356fe33bd7623b5
102096276 <102096276@student.swin.edu.au>
2022-09-12 15:18:58 +1000

    BREAKING CHANGE: file set up
    file set up for second half of section B

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import Todo from './components/Todo';
 function App() {
-  return (
-    <div>
-        <h1>My Todos</h1>
-        <Todo text="Here is a title"/>
-        <Todo text="Another title"/>
-    </div>
-  );
+  return<div></div>;
 }
 
 export default App;
